b16f4af738ce5d157d87485d54d328b2ffc168f9
asmae <rajae@LAPTOP-20AKIDH8>
2021-11-17 18:32:30 +0100

    Devoir 04, Exercice 1.

---
+++ MonEspacedeTravail/src/tp04/BoucleConditionnelle1.java
@@ -0,0 +1,25 @@
+package tp04;
+
+/** Exercice 1: boucle à posteriori 
+ * permet de répéter un traitement à l'aide de do et while
+ *  
+ * @author Asmae
+ *
+ */
+public class BoucleConditionnelle1 {
+
+	public static void main(String[] args) {
+		String s1 = "azerty", s2 = "ytreza";
+		boolean flag = false;
+		int i = 0, n = s1.length();
+		var strlen = (s1.length() == s2.length());
+		
+		do {System.out.println(flag);
+			if (s1.charAt(i) != s2.charAt(n - i - 1))
+					break;
+				i++;
+			if (i == n)
+				flag = true; }
+		while (i < n);
+	}
+}
